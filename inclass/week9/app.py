59cb4b184de19c3dda78c1519f951e98d54d622f
ChuckVisuals <firstinmath2@gmail.com>
2025-04-18 14:56:00 -0400

    test

---
+++ inclass/week9/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# MockData
+mock = [
+   {"game": "val",
+    "description": "description",
+    "hours": 300},
+   {"game": "minecraft",
+    "description": "description",
+    "hours": 50},
+   {"game": "FMV",
+    "description": "description",
+    "hours": 1000}
+]
+
+# Helper
+def find(corn_id):
+    pass
+
+# GET /items - Read all corn
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(mock), 200
+
+# POST /items return based on hours
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    
+    print(data)
+    
+    hours = data['hours']
+
+    for m in mock:
+        print(m)
+        if m['hours'] >= int(hours):
+            return jsonify(m), 202
+    return jsonify(), 404
+
+# PUT /items/<id> - Update info
+@app.route('/add_items', methods=['PUT'])
+def update_item():
+    
+    data = request.get_json()
+    
+    print(data.keys())
+    
+    if data.keys() == {'game', 'description', 'hours'}:
+        mock.append(data)
+        return jsonify(mock), 200
+    else:
+        return jsonify(), 400
+
+    data = request.json
+    #update info
+    pass
+
+# DELETE
+@app.route('/items/<int:corn_id>', methods=['DELETE'])
+def delete_item(corn_id):
+    global corn_field
+    # return 404 if item not found
+
+    # remove item and return 200
+    pass
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
